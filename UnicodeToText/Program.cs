1e9a580506b2c20efae72757bb3f731a1ed29b68
Ar6yZuK <Ar6yZuK@gmail.com>
2024-03-14 20:31:53 +0300

    Добавьте файлы проекта.

---
+++ UnicodeToText/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+while (true)
+{
+	WriteTitle();
+
+	string? str = Console.ReadLine();
+	if (str == null)
+		return;
+
+	string text = Regex.Unescape(str);
+
+	Console.WriteLine(text);
+}
+static void WriteTitle()
+{
+	Console.Write("Write unicode text like this(");
+	Console.ForegroundColor = ConsoleColor.Yellow;
+	Console.Write(@"\u041F\u043E\u043F\u0440\u043E\u0433\u0430\u0442\u044C \u002B(\u0447\u0430\u0441\u043E\u0432)");
+	Console.ResetColor();
+	Console.WriteLine("):");
+}
